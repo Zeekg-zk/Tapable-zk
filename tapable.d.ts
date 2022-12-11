98a608c13631be6847e0e452318db6f812c1d2dd
Zeekg <3193373997@qq.com>
2022-12-11 16:09:01 +0800

    feat:更新tapable的类型描述

--- tapable.d.ts
+++ tapable.d.ts
@@ -1,15 +1,47 @@
-type AsArray<T> = T extends any[] ? T : [T];
-
-type TapOptions = {
-  before?: number;
-  stage?: number;
+type TapType = "sync" | "async" | "promise";
+type HookCompileOptions = {
+  type: TapType;
 };
+type TFunction = (...args: any[]) => any;
 
-type Tap = TapOptions & {
+type Tap<T extends TapType = TapType> = {
   name: string;
+  type: T;
+  fn: TapFunction<T>;
+  stage?: number;
+  context?: boolean;
+  before?: string | string[];
 };
 
-type FullTap = Tap & {
-  type: "sync" | "async" | "promise";
-  fn: Function;
-};
+type TapFunction<T extends TapType = TapType> = T extends "sync"
+  ? (...args: any[]) => any
+  : T extends "async"
+  ? (...args: any[]) => void
+  : T extends "promise"
+  ? (...args: any[]) => Promise<any>
+  : never;
+
+type TapOptions<T extends TapType = TapType> = {
+  name: string;
+  stage?: number;
+  context?: boolean;
+  before?: string | string[];
+} & (T extends "sync"
+  ? {
+      type?: "sync";
+      fn?: TapFunction<"sync">;
+    }
+  : T extends "async"
+  ? {
+      type?: "async";
+      fn?: TapFunction<"async">;
+    }
+  : T extends "promise"
+  ? {
+      type?: "promise";
+      fn?: TapFunction<"promise">;
+    }
+  : {
+      type?: T;
+      fn?: TapFunction<T>;
+    });
